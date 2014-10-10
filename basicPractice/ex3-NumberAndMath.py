487baeab31525d6dae70a918b996140b26257cea
charlessong0@gmail.com <charlessong0@gmail.com>
2014-10-10 13:00:11 -0700

    ex4

---
+++ basicPractice/ex3-NumberAndMath.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+'''
+let's have some test on the numebrs and math. cool!
+'''
+
+print ' the following is the result of 1+1',1+1
+print 'alright! these tests are toooo simple for me, just last one about boolean judgement 5<3',     5<3
---
+++ basicPractice/ex4-Variables.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+test = 9
+population = 100
+double = 2.0
+
+final = test + double
+print test-population
+print test/double
+print final
