ede8bde59a7a6e711a597a7c5c23a89a2161d7a6
charlessong0@gmail.com <charlessong0@gmail.com>
2014-10-16 19:09:09 -0700

    test5 and 6

---
+++ basicPractice/ex5-VariablesString.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+name = 'Charles Song' # this is my real name!
+birthyear = 1991
+thisyear = 2014
+age = 23
+words = 'it is not a hard way to learn python here!'
+
+print "my full name is %s" %name
+print " and my age is %d, it is %d and is it correct?" %(age, thisyear - birthyear), age == (thisyear - birthyear)
+print "so %s\'s words are as below: %s" %(name, words)
+
---
+++ basicPractice/ex6-textAndstring.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+test1 = 'this is a test'
+test2 = 'this is another test'
+
+print test1+test2
+print test1,test2
+
+print test1+"and another?"
+print test2, "and and another?"
