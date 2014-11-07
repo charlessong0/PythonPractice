5ae0fd9444b3c0ab553be54c8bf985b2d1861acf
charlessong0@gmail.com <charlessong0@gmail.com>
2014-11-07 12:05:39 -0800

    ex19

---
+++ basicPractice/ex19-functions.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+def printnumbers(number1, number2):
+	print number1
+	print number2
+
+
+printnumbers(10+20, 33)
+
+test1 = 1
+test2 = 2
+
+printnumbers(test1*100, test2*100)
