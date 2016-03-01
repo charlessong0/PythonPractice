cee7728b4cb32446955936a35d00fd28d2040c63
Ubuntu <ubuntu@ip-172-31-18-107.us-west-2.compute.internal>
2016-03-01 21:23:50 +0000

    test

--- ironhack/data_analysis/network_generator.py
+++ ironhack/data_analysis/network_generator.py
@@ -3,12 +3,24 @@ import numpy as np
 import string
 import xlrd
 
+'''
+book = xlrd.open_workbook("test.xlsx")
+print "The number of worksheets is", book.nsheets
+print "Worksheet name(s):", book.sheet_names()
+sh = book.sheet_by_index(2)
+sh0 = book.sheet_by_index(0)
+sh3 = book.sheet_by_index(3)
+print sh.name, sh.nrows, sh.ncols
+print "Cell D30 is", sh.cell_value(rowx=29, colx=3)
+'''
+
 dt = [('len', float)]
 
-A = np.array([(0, 0.3, 0.4, 0.7),
-               (0.3, 0, 0.9, 0.2),
-               (0.4, 0.9, 0, 0.1),
-               (0.7, 0.2, 0.1, 0)
+A = np.array([(0.3, 0, 0.3, 0.4, 0.7),
+               (0.3, 0.6,  0, 0.9, 0.2),
+               (0.4, 0.9, 0.4, 0, 0.1),
+               (0.7, 0.2, 0.1, 0.1, 0),
+               (0.3, 0.9, 0.9, 0.5, 0.1)		
                ])*10
 A = A.view(dt)
 
