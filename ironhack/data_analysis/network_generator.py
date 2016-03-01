61c377c3a8a8f24e6572b6056055f21d0afd09bf
Ubuntu <ubuntu@ip-172-31-18-107.us-west-2.compute.internal>
2016-03-01 21:02:03 +0000

    test

--- ironhack/data_analysis/network_generator.py
+++ ironhack/data_analysis/network_generator.py
@@ -20,6 +20,6 @@ G = nx.drawing.nx_agraph.to_agraph(G)
 G.node_attr.update(color="red", style="filled")
 G.edge_attr.update(color="blue", width="2.0")
 
-G.draw('/tmp/out.png', format='png', prog='neato')
+G.draw('out.png', format='png', prog='neato')
 
 
