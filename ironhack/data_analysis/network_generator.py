e4aad85b9c0cafe03a17ef36a3846d364fc88e21
Ubuntu <ubuntu@ip-172-31-18-107.us-west-2.compute.internal>
2016-03-01 22:19:18 +0000

    test

--- ironhack/data_analysis/network_generator.py
+++ ironhack/data_analysis/network_generator.py
@@ -13,9 +13,11 @@ sh = book.sheet_by_index(0)
 print sh.name, sh.nrows, sh.ncols
 print "Cell D30 is", sh.cell_value(rowx=3, colx=3)
 
+names = [0 for x in range(sh.nrows-1)]
 array = [[0 for x in range(sh.nrows-1)] for x in range(sh.ncols-1)]
 
 for i in range(1, sh.nrows):
+	names[i-1] = sh.cell_value(0, i)
 	for j in range(1, sh.ncols):
 		array[i-1][j-1] = sh.cell_value(rowx=i, colx=j)
 print array
@@ -32,7 +34,8 @@ A = np.array(array)
 A = A.view(dt)
 
 G = nx.from_numpy_matrix(A)
-G = nx.relabel_nodes(G, dict(zip(range(len(G.nodes())),string.ascii_uppercase)))    
+#G = nx.relabel_nodes(G, dict(zip(range(len(G.nodes())),string.ascii_uppercase)))    
+G = nx.relabel_nodes(G, dict(zip(range(len(G.nodes())),names)))
 
 G = nx.drawing.nx_agraph.to_agraph(G)
 
