f0e9c9e9ab0c822910e8fbd89109d37b22a44f5b
Ubuntu <ubuntu@ip-172-31-18-107.us-west-2.compute.internal>
2016-03-02 17:12:06 +0000

    without names network graph

--- ironhack/data_analysis/network_generator.py
+++ ironhack/data_analysis/network_generator.py
@@ -34,14 +34,14 @@ A = np.array(array)
 A = A.view(dt)
 
 G = nx.from_numpy_matrix(A)
-#G = nx.relabel_nodes(G, dict(zip(range(len(G.nodes())),string.ascii_uppercase)))    
-G = nx.relabel_nodes(G, dict(zip(range(len(G.nodes())),names)))
+G = nx.relabel_nodes(G, dict(zip(range(len(G.nodes())),string.ascii_uppercase)))    
+#G = nx.relabel_nodes(G, dict(zip(range(len(G.nodes())),names)))
 
 G = nx.drawing.nx_agraph.to_agraph(G)
 
 G.node_attr.update(color="red", style="filled")
 G.edge_attr.update(color="blue", width="2.0")
 
-G.draw('out.png', format='png', prog='neato')
+G.draw('output.png', format='png', prog='neato')
 
 
