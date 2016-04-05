ca4f2ef550bb0d6740b843ff5f670759f947106d
Charles <charlessong0@gmail.com>
2016-04-05 10:43:12 -0700

    intercoder reliability

---
+++ ironhack/intercoder_reliability/class_intercoder.py
@@ -0,0 +1,52 @@
+import xlrd
+import xlwt
+import glob
+import os
+
+file_list = glob.glob("data/*.xls")
+wb = xlwt.Workbook()
+ws = wb.add_sheet("intercoder_matrix", cell_overwrite_ok=True)
+
+index_x = 1
+index_y = 1
+num_matching = 0
+num_total = 0
+
+#temp_path = ""
+for path1 in file_list:
+    book1 = xlrd.open_workbook(path1)
+    sh1 = book1.sheet_by_index(0)
+
+    for path2 in file_list:
+        '''
+        print path1
+        print "==="
+        print path2
+        print "============="
+        '''
+        book2 = xlrd.open_workbook(path2)
+        sh2 = book2.sheet_by_index(0)
+        #x range: 2-4; y range: 50-54, 58-59, 63-68
+        for i in range(2, 5):
+            for j in range(50, 55):
+                if (sh1.cell_value(rowx=i, colx=j) == sh2.cell_value(rowx=i, colx=j)):
+                   num_matching = num_matching + 1 
+                num_total = num_total + 1
+            for j in range(58, 60):
+                if (sh1.cell_value(rowx=i, colx=j) == sh2.cell_value(rowx=i, colx=j)):
+                   num_matching = num_matching + 1 
+                num_total = num_total + 1
+            for j in range(63, 69):
+                if (sh1.cell_value(rowx=i, colx=j) == sh2.cell_value(rowx=i, colx=j)):
+                   num_matching = num_matching + 1 
+                num_total = num_total + 1
+        #write into output
+        ws.write(index_x, index_y, num_matching/num_total)
+        #swich to next y and reset the numbers
+        num_matching = 0
+        num_total = 0
+        index_x = index_x+1
+
+    index_y = index_y+1
+
+wb.save("output.xls")
